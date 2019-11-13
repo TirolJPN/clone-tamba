742f297a6dc4602fde11f242dcb87cb30305c65a
TirolJPN <tirol.jpn@gmail.com>
2019-11-13 17:24:49 +0900

    fix var name on root.go

--- cmd/fetch.go
+++ cmd/fetch.go
@@ -1,14 +1,9 @@
 package cmd
 
 import (
-	"fmt"
 	"github.com/spf13/cobra"
 )
 
-var RootCmd = &cobra.Command{
-	Use:   "culc",
-	Short: "command line calculator",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("root command")
-	},
+func fetchCmd() *cobra.Command {
+
 }
